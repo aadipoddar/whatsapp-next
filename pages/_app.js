47f384ef997a48ed56ecb9f3722ceabef5539f77
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-04 21:36:46 +0530

    Login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,12 @@ import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Login from './Login'
 import { auth, db } from '../firebase'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth)
+
+  if (true) return <Loading />
 
   if (!user) return <Login />
 
