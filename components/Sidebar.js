a86cd584c07fc21c0434ab8ff7b0e5f1ec39ca60
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-05 16:08:51 +0530

    Validate Email and Store email Address

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,22 +1,39 @@
 import styled from 'styled-components'
+import * as EmailValidator from 'email-validator'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { auth, db } from '../firebase'
+
 import { Avatar, IconButton, Button } from '@material-ui/core'
 
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/Search'
-import { auth } from '../firebase'
 
 function Sidebar() {
+
+    const [user] = useAuthState(auth)
+
+    const userChatRef = db.collection('chats').where('users', 'array-contains', user.email)
+    const [chatsSnapshot] = useCollection(userChatRef)
 
     const createChat = () => {
         const input = prompt('Please Enter the email address for the user you wish to chat!')
 
         if (!input) return null
 
-        if (EmailValidator.validate(input)) {
-            // TODO - We need to add the chat in the database
+        if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input != user.email) {
+            db.collection('chats').add({
+                users: [user.email, input]
+            })
         }
     }
+
+    const chatAlreadyExists = (recipientEmail) =>
+        !!chatsSnapshot?.docs
+            .find(chat => chat.data().users
+                .find(user => user == recipientEmail)?.length > 0)
+
 
     return (
         <Container>
