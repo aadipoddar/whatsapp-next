912acf1db0f725bb8fd007b3874e5d67155b581d
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-05 15:46:33 +0530

    Storing User Details

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import '../styles/globals.css'
 import Login from './Login'
-import { auth, db } from '../firebase'
 import Loading from '../components/Loading'
+import { auth, db } from '../firebase'
+import firebase from 'firebase'
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoURL: user.photoURL
+      }, { merge: true })
+    }
+  }, [user])
 
   if (loading) return <Loading />
 
